1018a749bf86bd575f273d8b380f253e62c61bef
Rodrigo Buges <99702723+rod-buges@users.noreply.github.com>
2022-11-23 00:41:03 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+   $dbHost = 'Localhost';
+   $dbUsername = 'root';
+   $dbPassword = '';
+   $dbName = 'formulario';
+
+   $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+   
+   //if($conexao->connect_errno){
+    //echo "Erro";
+   //}else{
+    //echo "Conexao efetuada com sucesso!";
+   //}
+?>
